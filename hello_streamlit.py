a80f11004363df2d71cb1a83fac908b00c741985
9AEUNNY <GAEUN030215@GMAIL.COM>
2024-11-25 17:11:23 +0900

    Add files via upload

--- hello_streamlit.py
+++ hello_streamlit.py
@@ -15,14 +15,13 @@ st.subheader('slider')
 like=st.slider('가은이를 얼만큼 조아하시나요',0,200,100)
 st.write("바로바로",like,'이만큼~~~~~')
 
+emotion=st.slider('오늘 기분은 몇 점??',0,100,0)
+st.write("오늘 기분",emotion,'점~~~~~ 힘을 내세용')
+
 option=st.selectbox('가장 좋아하는 음식은?',('방어','타코','삼겹살'))
 st.write("당신이 제일 좋아하는 음식은",option,'입니당')
 
-options=st.multiselect(
-    '가장 좋아하는 색깔은 무엇인가요',
-    ['초록','노랑','검정','빨강','파랑']
-    ['노랑','빨강']
-)
+st.write("다음 간식 중 가장 맛있는 것은?")
 
 icecream=st.checkbox('아이스크림')
 coffee=st.checkbox('커피')
